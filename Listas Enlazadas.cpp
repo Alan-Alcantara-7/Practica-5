672abad67aac4173d58416419f51fe31b61b3218
Alan Alcantara <alcantaraplataas@gmail.com>
2019-09-13 13:25:23 -0500

    Codigo con vista Dinamica

--- Listas Enlazadas.cpp
+++ Listas Enlazadas.cpp
@@ -10,7 +10,60 @@ struct Nodo{
 	}
 };
 Nodo D1(13), D2(7), D3(11), D4(15), D5(2);
+
 int main(int argc, char** argv) {
+	struct Nodo *inicio;
+	struct Nodo *aux;
+	inicio=NULL;
+	aux = new Nodo(0);
+	
+	cout<<"Valores de Nodo:"<<endl;
+	aux->valor=3;
+	aux->ptr=NULL;
+	cout<<aux->valor<<endl;
+	cout<<endl;
+	
+	
+	inicio = aux;
+	aux = new Nodo(0);
+	aux->valor=5;
+	aux->ptr=NULL;
+	cout<<aux->valor<<endl;
+	cout<<endl;
+	
+	
+	inicio->ptr=aux;
+	aux = new Nodo(0);
+	aux->valor=12;
+	aux->ptr=NULL;
+	cout<<aux->valor<<endl;
+	cout<<endl;
+	
+	
+	inicio->ptr->ptr=aux;
+	aux = new Nodo(0);
+	aux->valor=35;
+	aux->ptr=NULL;
+	cout<<aux->valor<<endl;
+	cout<<endl;
+	
+	
+	inicio->ptr->ptr->ptr=aux;
+	aux = new Nodo(0);
+	aux->valor=64;
+	aux->ptr=NULL;
+	cout<<aux->valor<<endl;
+	cout<<endl;
+	
+	
+	inicio->ptr->ptr->ptr->ptr=aux;
+	aux = new Nodo(0);
+	aux->valor=89;
+	aux->ptr=NULL;
+	cout<<aux->valor<<endl;
+	cout<<endl;
+	
+	cout<<"Otros valores:"<<endl;
 	cout<<D1.valor<<endl;
 	cout<<endl;
 	cout<<D2.valor<<endl;
@@ -30,5 +83,13 @@ int main(int argc, char** argv) {
 	cout<<(D1.ptr)->ptr->valor<<endl;
 	cout<<(D1.ptr)->ptr->ptr->valor<<endl;
 	cout<<(D1.ptr)->ptr->ptr->ptr->valor<<endl;
+	
+	cout<<"Vista Dinamica:"<<endl;
+	cout<<inicio->valor<<endl;
+	cout<<(inicio->ptr)->valor<<endl;
+	cout<<(inicio->ptr)->ptr->valor<<endl;
+	cout<<(inicio->ptr)->ptr->ptr->valor<<endl;
+	cout<<(inicio->ptr)->ptr->ptr->ptr->valor<<endl;
+	
 	 return 0;
 }
