189651d6db5ffe615e552b8c583cf4f21beb384a
Alan Alcantara <alcantaraplataas@gmail.com>
2019-09-20 13:04:58 -0500

    Estructura donde se crean nodos apartir de while

--- Menu Listas.cpp
+++ Menu Listas.cpp
@@ -6,16 +6,15 @@ using namespace std;
 struct Nodo{
 	int valor;
 	Nodo *ptr;
-	Nodo(int dato1){
-		valor = dato1;
-		ptr = NULL;
-	}
 };
-Nodo D1(13), D2(7), D3(11), D4(15), D5(2);
-void agregar(struct Nodo*, int);
+void agregar( int);
+void mostrar();
+struct Nodo *lista=NULL;
 
 int main(int argc, char** argv) {
-	int opcion;
+	int opcion, add;
+	while(opcion!=3){
+	
 	cout<<"MENU"<<endl;
 	cout<<"1. Llenar Nodos"<<endl;
 	cout<<"2. Mostar Datos de Nodos"<<endl;
@@ -23,11 +22,16 @@ int main(int argc, char** argv) {
 	cin>>opcion;
 	switch (opcion){
         case 1:
-        	cout<<"Llenado de Nodos";
-        	agregar(inicioLista, valor)
-		break;
+        	cout<<"Llenado de Nodos"<<endl;
+        	cin>>add;
+        	cout<<endl;
+        	agregar(add);
+        	
+			break;
         case 2:
-            cout<<"Mostra los datos de Nodos ";
+            cout<<"Mostrar los datos de Nodos ";
+            cout<<endl;
+            mostrar();
             break;
         case 3:
         	cout<<"Salir";
@@ -35,91 +39,31 @@ int main(int argc, char** argv) {
             cout<<"Gracias"<<endl;
             break;
     }
-	struct Nodo *inicio;
-	struct Nodo *aux;
-	inicio=NULL;
-	aux = new Nodo(0);
-	
-	cout<<"Valores de Nodo:"<<endl;
-	aux->valor=3;
-	aux->ptr=NULL;
-	cout<<aux->valor<<endl;
-	cout<<endl;
-	
-	
-	inicio = aux;
-	aux = new Nodo(0);
-	aux->valor=5;
-	aux->ptr=NULL;
-	cout<<aux->valor<<endl;
-	cout<<endl;
-	
-	
-	inicio->ptr=aux;
-	aux = new Nodo(0);
-	aux->valor=12;
-	aux->ptr=NULL;
-	cout<<aux->valor<<endl;
-	cout<<endl;
-	
-	
-	inicio->ptr->ptr=aux;
-	aux = new Nodo(0);
-	aux->valor=35;
-	aux->ptr=NULL;
-	cout<<aux->valor<<endl;
-	cout<<endl;
-	
-	
-	inicio->ptr->ptr->ptr=aux;
-	aux = new Nodo(0);
-	aux->valor=64;
-	aux->ptr=NULL;
-	cout<<aux->valor<<endl;
-	cout<<endl;
-	
-	
-	inicio->ptr->ptr->ptr->ptr=aux;
-	aux = new Nodo(0);
-	aux->valor=89;
-	aux->ptr=NULL;
-	cout<<aux->valor<<endl;
-	cout<<endl;
-	
-	cout<<"Otros valores:"<<endl;
-	cout<<D1.valor<<endl;
-	cout<<endl;
-	cout<<D2.valor<<endl;
-	cout<<endl;
-	cout<<D3.valor<<endl;
-	
-	D1.ptr=&D2;
-	D2.ptr=&D3;
-	D3.ptr=&D4;
-	D4.ptr=&D5;
-	D5.ptr=NULL;
-	cout<<endl;
-	
-	cout<<"Valor de puntero:"<<endl;
-	cout<<D1.valor<<endl;
-	cout<<(D1.ptr)->valor<<endl;
-	cout<<(D1.ptr)->ptr->valor<<endl;
-	cout<<(D1.ptr)->ptr->ptr->valor<<endl;
-	cout<<(D1.ptr)->ptr->ptr->ptr->valor<<endl;
-	
-	cout<<"Vista Dinamica:"<<endl;
-	cout<<inicio->valor<<endl;
-	cout<<(inicio->ptr)->valor<<endl;
-	cout<<(inicio->ptr)->ptr->valor<<endl;
-	cout<<(inicio->ptr)->ptr->ptr->valor<<endl;
-	cout<<(inicio->ptr)->ptr->ptr->ptr->valor<<endl;
+}
 	
 	 return 0;
 }
-void agregar(struct Nodo* inicioLista, int valor)
+void agregar( int add)
 {
-	struct Nodo *auxiliar;
-	auxiliar=inicioLista;
-	auxiliar++;
+	Nodo *nuevo= new Nodo;
+	nuevo->ptr=NULL;
+	nuevo->valor=add;
+	if(lista==NULL)lista=nuevo;
+	else{
+		Nodo *aux=lista;
+		while(aux->ptr=NULL){
+			aux=aux->ptr;
+		}
+		lista->ptr=nuevo;
+	}
+	cout<<lista->valor<<" "<<endl;
+}
+void mostrar(){
+	Nodo *aux=lista;
+	while(aux!=NULL){
+		cout<<aux->valor<<" ";
+		aux=aux->ptr;
+	}
+	cout<<endl;
 }
 
